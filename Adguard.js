f855f926c1a947b1e525d7f0b058cd565bb4f6cb
苌涛 <changziran@gmail.com>
2021-04-01 23:37:56 +0800

    update

---
+++ Adguard.js
@@ -0,0 +1,4 @@
+hostname=mobile-api.adguard.com
+
+#Adguard
+^https:\/\/mobile-api\.adguard\.com\/api\/1\.0\/ios_validate_receipt$ url 302 https://raw.githubusercontent.com/langkhach270389/Scripting/master/Adguard.rsps
--- Scripts/qyxj.js
+++
@@ -1,9 +0,0 @@
-
- */
-let obj = JSON.parse($response.body);
-obj.data.end_time=3725012184;
-obj.data.is_cancel_subscribe=false;
-obj.data.flag=true;
-$done({body: JSON.stringify(obj)});
-
-//轻颜相机
